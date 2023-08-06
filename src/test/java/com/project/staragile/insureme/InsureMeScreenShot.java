3cbc7a0ed43faf3d81750217394308ec64830f2e
RAHUL KUMAR <rahulpatna89@gmail.com>
2023-08-06 19:14:30 +0530

    Update InsureMeScreenShot.java

--- src/test/java/com/project/staragile/insureme/InsureMeScreenShot.java
+++ src/test/java/com/project/staragile/insureme/InsureMeScreenShot.java
@@ -29,7 +29,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 @SuppressWarnings("unchecked")
-public class insureMe {
+public class InsureMeScreenShot {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
                    WebDriverManager.chromedriver().setup();
